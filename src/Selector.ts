041d89a55eabc063f374d3dfa7560318847f076c
ludeeus <joasoe@gmail.com>
2019-10-16 16:10:41 +0000

    update

--- src/Selector.ts
+++ src/Selector.ts
@@ -9,9 +9,12 @@ import { PullLabler } from "./packs/PullLabler"
 
 export async function Selector(select: string, client: github.GitHub) {
     const repository: string = core.getInput('repository')
-    const categoty: string = core.getInput('repository')
+    const categoty: string = core.getInput('categoty')
 
-    if (repository && categoty) CheckRepository(repository, categoty);
+    if (repository && categoty) {
+        CheckRepository(repository, categoty);
+        return
+    }
 
     if (select.length === 0) {
         core.setFailed("You need to add an action.");
