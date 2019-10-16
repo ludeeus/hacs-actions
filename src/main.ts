24fca2db3a61d51526d0113a2df9622d5d809879
ludeeus <joasoe@gmail.com>
2019-10-16 18:02:24 +0000

    update

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ async function run() {
   try {
     const client = new github.GitHub(core.getInput('github-token', {required: true}));
     await Selector(core.getInput('action'), client)
+
+
+    var issue_data = await client.issues.get({"owner": "ludeeus", "repo": "hacs-actions", "issue_number": 3})
+    console.log(issue_data)
+
+
   } catch (error) {
     core.setFailed(error.message);
   }
