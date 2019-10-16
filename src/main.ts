d5487d4803f71666a7c31d3839cacdf99a6483c9
ludeeus <joasoe@gmail.com>
2019-10-16 18:05:46 +0000

    update

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,16 @@ import { Selector } from "./Selector"
 async function run() {
   try {
     const client = new github.GitHub(core.getInput('github-token', {required: true}));
+
+
+    if (core.getInput('action') === "Test") {
+      var issue_data = await client.issues.get({"owner": "ludeeus", "repo": "hacs-actions", "issue_number": github.context.issue.number})
+      console.log(issue_data.data.state)
+    }
+
+
+
     await Selector(core.getInput('action'), client)
-
-
-    var issue_data = await client.issues.get({"owner": "ludeeus", "repo": "hacs-actions", "issue_number": 3})
-    console.log(issue_data)
-
 
   } catch (error) {
     core.setFailed(error.message);
