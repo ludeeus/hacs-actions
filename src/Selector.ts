6e3683244470abbc0f2d7d84803dc3d65159c06a
ludeeus <joasoe@gmail.com>
2019-10-19 21:01:01 +0000

    update

--- src/Selector.ts
+++ src/Selector.ts
@@ -7,7 +7,7 @@ import { PullLabler } from "./packs/PullLabler"
 import { HacktoberFest } from "./packs/HacktoberFest"
 
 
-export async function Selector(modules: string, client: github.GitHub) {
+export async function Selector(client: github.GitHub) {
     const repository: string = core.getInput('repository')
     const categoty: string = core.getInput('categoty')
 
@@ -16,35 +16,24 @@ export async function Selector(modules: string, client: github.GitHub) {
         return
     }
 
-    if (modules.length === 0) {
-        core.setFailed("You need to add an action.");
-    }
-
-    const ComputedModules: string[] = modules.split(",")
-    if (github.context.payload.issue !== undefined) await IssueActions(ComputedModules, client);
-    if (github.context.payload.pull_request !== undefined) await PullRequestActions(ComputedModules, client);
+    if (github.context.payload.issue !== undefined) await IssueActions(client);
+    if (github.context.payload.pull_request !== undefined) await PullRequestActions(client);
 }
 
 
-async function IssueActions(modules: string[], client: github.GitHub) {
+async function IssueActions(client: github.GitHub) {
     const issue = github.context.payload.issue;
 
-    modules.forEach(async function(module: string) {
-        if (module.length !== 0) {
-            if (module.trim() === "IssueGreeter") await IssueGreeter(client);
-            if (module.trim() === "HacktoberFest") await HacktoberFest(client);
-        }
-    })
+    await IssueGreeter(client);
+    await HacktoberFest(client);
+
 }
 
-async function PullRequestActions(modules: string[], client: github.GitHub) {
+async function PullRequestActions(client: github.GitHub) {
     const pull = github.context.payload.pull_request;
 
-    modules.forEach(async function(module: string) {
-        if (module.length !== 0) {
-            if (module.trim() === "PullLabler") await PullLabler(client);
-            if (module.trim() === "PullGreeter") await PullGreeter(client);
-            if (module.trim() === "HacktoberFest") await HacktoberFest(client);
-        }
-    })
+    await PullLabler(client);
+    await PullGreeter(client);
+    await HacktoberFest(client);
+
 }
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function run() {
   try {
     const client = new github.GitHub(core.getInput('github-token', {required: true}));
 
-    await Selector(core.getInput('modules'), client)
+    await Selector(client)
 
   } catch (error) {
     core.setFailed(error.message);
