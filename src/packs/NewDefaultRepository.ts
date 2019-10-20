91a7758b281d8d5fa2f8e94793c32b6b5ce1b6db
ludeeus <joasoe@gmail.com>
2019-10-20 11:39:01 +0000

    update

--- src/packs/NewDefaultRepository.ts
+++ src/packs/NewDefaultRepository.ts
@@ -8,6 +8,10 @@ export async function NewDefaultRepository(client: github.GitHub) {
 
         // Get Changed files
         var changedFiles = await getChangedFiles(client, Issue.number)
+
+        changedFiles.forEach(async element => {
+            await getFileDiff(client, Issue.number, element)
+        });
     }
 }
 
@@ -21,9 +25,41 @@ async function getChangedFiles(client: github.GitHub, prNumber: number){
       pull_number: prNumber
     });
   
-    console.log(listFilesResponse)
-    const changedFiles = listFilesResponse.data.map(f => {f.filename, f.contents_url});
+    const changedFiles = listFilesResponse.data.map(f => f.filename);
     console.log(changedFiles)
   
     return changedFiles;
+  }
+
+async function getFileDiff(client: github.GitHub, prNumber: number, file: string){
+
+    const {data: Pull} = await client.pulls.get({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: prNumber
+    })
+
+    const PullRef = Pull["head"]["sha"]
+
+    const {data: ChangedContents} = await client.repos.getContents({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        path: file,
+        ref: PullRef
+    })
+
+    var ChangedDecoded = JSON.parse(new Buffer(ChangedContents["content"], 'base64').toString('utf-8'));
+
+    const {data: Contents} = await client.repos.getContents({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        path: file,
+        ref: PullRef
+    })
+
+    var Decoded = JSON.parse(new Buffer(Contents["content"], 'base64').toString('utf-8'));
+
+
+    console.log(ChangedDecoded)
+    console.log(Decoded)
   }
