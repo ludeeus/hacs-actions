5cb44ea012d485d733d09e1cb656042cbb4db0df
ludeeus <joasoe@gmail.com>
2019-10-20 11:14:20 +0000

    update

--- src/packs/NewDefaultRepository.ts
+++ src/packs/NewDefaultRepository.ts
@@ -1,19 +1,29 @@
+import * as core from '@actions/core';
 import * as github from '@actions/github';
 import { Issue } from "../misc/contexts"
 
 export async function NewDefaultRepository(client: github.GitHub) {
-    console.log("Running NewDefaultRepository")
     if (github.context.repo.owner !== "hacs" && github.context.repo.repo !== "repositories") {
+        console.log("Running NewDefaultRepository")
 
-        // Get diff
-        const Diff = await client.pulls.get({
-            owner: Issue.owner,
-            repo: Issue.repo,
-            pull_number: Issue.number,
-            mediaType: {
-              format: "diff"
-            }
-          });
-          console.log(Diff)
+        // Get Changed files
+        var changedFiles = await getChangedFiles(client, Issue.number)
     }
 }
+
+
+
+
+async function getChangedFiles(client: github.GitHub, prNumber: number){
+    const listFilesResponse = await client.pulls.listFiles({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: prNumber
+    });
+  
+    console.log(listFilesResponse)
+    const changedFiles = listFilesResponse.data.map(f => f.filename);
+    console.log(changedFiles)
+  
+    return changedFiles;
+  }
