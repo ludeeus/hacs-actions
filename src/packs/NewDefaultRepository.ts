fe4c8d21163bf470664e4cf5232858acac11029b
ludeeus <joasoe@gmail.com>
2019-10-20 11:07:53 +0000

    update

--- src/packs/NewDefaultRepository.ts
+++ src/packs/NewDefaultRepository.ts
@@ -6,7 +6,7 @@ export async function NewDefaultRepository(client: github.GitHub) {
     if (github.context.repo.owner !== "hacs" && github.context.repo.repo !== "repositories") {
 
         // Get diff
-        const { data: Diff } = await client.pulls.get({
+        const Diff = await client.pulls.get({
             owner: Issue.owner,
             repo: Issue.repo,
             pull_number: Issue.number,
