a538afe5d53b367a9c7fb9bdea1d85a4211ca728
ludeeus <joasoe@gmail.com>
2019-10-20 15:37:24 +0000

    update

--- src/checks/PluginCheck.ts
+++ src/checks/PluginCheck.ts
@@ -1,23 +1,104 @@
 import * as github from '@actions/github';
 import * as core from '@actions/core';
+import { Base64 } from 'js-base64';
 import { Issue } from "../misc/contexts"
 
 export async function PluginCheck(owner: string, repo: string, client: github.GitHub) {
+    await CheckImportType(owner, repo, client)
+    await CheckPluginLocation(owner, repo, client)
 
+}
+
+
+async function CheckImportType(owner: string, repo: string, client: github.GitHub) {
     try {
-        await client.repos.getContents({
+        var BaseFiles = await client.repos.getContents({
             owner: owner,
             repo: repo,
-            path: "themes"
-        })
-        core.info(`✅  Theme dir exist`);
+            path: ""
+        });
+
+        var readme: string = "";
+        (BaseFiles.data as [any]).forEach(element => {
+            if (String(element.name).toLowerCase() === "readme") readme = Base64.decode(element.content);
+            if (String(element.name).toLowerCase() === "readme.md") readme = Base64.decode(element.content);
+        });
+
+        if (readme.length >0) {
+            if (readme.includes("type: module") || readme.includes("type: js")) {
+                core.info(`✅  JS import type defined`);
+            } else {
+                throw "error"
+            }
+        }
+
     } catch (error) {
-        core.setFailed(`❌  Theme dir exist`);
+        core.setFailed(`❌  JS import type defined`);
         await client.issues.createComment({
             owner: Issue.owner,
             repo: Issue.repo,
             issue_number: Issue.number,
-            body: `Integration does not exist in the [themes directory](https://hacs.xyz/docs/publish/theme#repository-structure)`
+            body: `The repository README does not have info about how the plugin should be defined.\nhttps://hacs.xyz/docs/publish/plugin#import-type`
+          });
+        return
+    }
+}
+
+async function CheckPluginLocation(owner: string, repo: string, client: github.GitHub) {
+    var pluginExist = false
+    const valid_names = [
+        `${repo.replace("lovelace-", "")}.js`,
+        `${repo}.js`,
+        `${repo}.umd.js`,
+        `${repo}-bundle.js`,
+    ]
+
+    try {
+        var DistContents = await client.repos.getContents({
+            owner: owner,
+            repo: repo,
+            path: "disc"
+        });
+
+        (DistContents.data as [any]).forEach(element => {
+            if (element.name.endswith(".js")) {
+                if (valid_names.includes(".js")) pluginExist = true
+            }
+        });
+
+        var ReleaseContents = await client.repos.getLatestRelease({
+            owner: owner,
+            repo: repo
+        });
+
+        (ReleaseContents.data.assets as [any]).forEach(element => {
+            if (element.name.endswith(".js")) {
+                if (valid_names.includes(".js")) pluginExist = true
+            }
+        });
+
+
+        var RootContents = await client.repos.getContents({
+            owner: owner,
+            repo: repo,
+            path: ""
+        });
+
+        (RootContents.data as [any]).forEach(element => {
+            if (element.name.endswith(".js")) {
+                if (valid_names.includes(".js")) pluginExist = true
+            }
+        });
+
+        if (!pluginExist) throw "error"
+        core.info(`✅  Plugin exist`);
+    } catch (error) {
+        core.setFailed(`❌  Plugin exist`);
+        await client.issues.createComment({
+            owner: Issue.owner,
+            repo: Issue.repo,
+            issue_number: Issue.number,
+            body: `The location of the plugin is in one of the expected locations\nhttps://hacs.xyz/docs/publish/plugin#repository-structure`
           });
         return
     }
